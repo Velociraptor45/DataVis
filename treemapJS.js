94e43d545dcb3ee2e9fc5c1f5ddd6d5f938189d3
David <dk@popkultur.bayern>
2018-04-29 17:58:53 +0200

    implemented building a suitable json and first parts of the treemap display
    Signed-off-by: David <dk@popkultur.bayern>

--- treemapJS.js
+++ treemapJS.js
@@ -1,5 +1,116 @@
+var d3 = d3 || {};
+
 (function(){
-    var json = d3.json("", function(data){
-        //...
-    });
-});
+    "use strict";
+
+    var treemapDataHome = [];
+    var treemapDataAway = [];
+
+    function loadJSON(){
+        d3.json("JSON/bundesliga06_17.json", function(data){
+            buildTreemapJSON(data.bundesliga[10].saison_16_17);
+            buildTreemap();
+        });
+    }
+
+    /*
+        data contains one season
+    */
+    function buildTreemapJSON(data){
+        for(var i = 0; i < data.length; i++){
+            var homeTeam = data[i]["HomeTeam"];
+            var awayTeam = data[i]["AwayTeam"]
+            if(!isInData(homeTeam, treemapDataHome)){
+                addKeyToJSON(homeTeam, treemapDataHome);
+            }
+            if(!isInData(awayTeam, treemapDataAway)){
+                addKeyToJSON(awayTeam, treemapDataAway);
+            }
+            switch(data[i]["FTR"]){
+                case 'H':  
+                    var index = getIndexOfTeam(homeTeam, treemapDataHome);
+                    if(index != -1){
+                        treemapDataHome[index].value += 1;
+                    }
+                    else{
+                        console.log("Indexfehler" + HomeTeam);
+                    }
+                    break;
+                case 'A':
+                var index = getIndexOfTeam(awayTeam, treemapDataAway);
+                if(index != -1){
+                    treemapDataAway[index].value += 1;
+                }
+                else{
+                    console.log("Indexfehler " + awayTeam);
+                }
+                    break;
+                default: ;
+            }
+        }
+        //console.log(treemapDataAway);
+        //console.log(treemapDataHome);
+    }
+
+    function addKeyToJSON(team, array)
+    {
+        var obj = {"team": team, "value": 0}
+        array.push(obj);
+    }
+
+    function getIndexOfTeam(team, data)
+    {
+        for(var i = 0; i < data.length; i++){
+            if(data[i].team == team){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function isInData(team, data){
+        for(var i = 0; i < data.length; i++){
+            if(data[i].team == team){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function buildTreemap()
+    {
+        console.log(treemapDataHome);
+        var rankingObject = {"children": treemapDataHome}
+
+        var svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height");
+
+        var treemap = d3.treemap()
+            .tile(d3.treemapResquarify)
+            .size([width, height])
+            .round(false)
+            .paddingInner(1);
+
+        var root = d3.hierarchy(rankingObject)
+            .sum(function(d){
+                return d.value;
+            })
+            .sort(function(a, b){
+                return b.value - a.value;
+            });
+
+        treemap(root);
+        addItemsToDOM(svg, root.children);
+    }
+
+    function addItemsToDOM(svg, data)
+    {
+        var rectangle = svg.selectAll("g")
+            .data(data)
+            .enter().append("g");
+    }
+
+
+    loadJSON();
+}());
